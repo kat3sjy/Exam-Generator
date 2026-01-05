28aa4101f144a624a2cf9792b68953ddea084ed7
kat3sjy <kateshen2006@gmail.com>
2026-01-05 03:22:47 -0500

    the basics

---
+++ exam generator project/Exam-Generator/src/components/FileUploader/FileUploader.tsx
@@ -0,0 +1,11 @@
+
+function FileUploader() {
+  return (
+    <div>
+        <label htmlFor="fileUpload">Upload File:</label>
+        <input type="file" id="fileUpload" name="fileUpload" />
+    </div>
+);
+}
+
+export default FileUploader;
---
+++ exam generator project/Exam-Generator/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import FileUploader from "../components/FileUploader/FileUploader";
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <FileUploader />
+        </div>
+
+    );
+}
+
+export default HomePage;
