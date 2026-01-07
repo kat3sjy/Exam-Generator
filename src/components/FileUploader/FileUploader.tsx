58476fbf6e796aa2dbb7c6ef664cdd4cc230de1c
kat3sjy <kateshen2006@gmail.com>
2026-01-06 22:30:37 -0500

    front end updates

--- src/components/FileUploader/FileUploader.tsx
+++ src/components/FileUploader/FileUploader.tsx
@@ -1,11 +1,21 @@
+import "./FileUploader.css";
+import React, { useState } from "react";
 
 function FileUploader() {
+
+  // const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files){
+      const file = e.target.files[0];
+    };
+  };
+
   return (
     <div>
-        <label htmlFor="fileUpload">Upload File:</label>
-        <input type="file" id="fileUpload" name="fileUpload" />
+      <label htmlFor="fileUpload" className="file-uploader">Upload File</label>
+      <input type="file" id="fileUpload" name="fileUpload" style={{ display: "none" }} onChange={handleFileChange}/>
     </div>
-);
+  );
 }
 
 export default FileUploader;
